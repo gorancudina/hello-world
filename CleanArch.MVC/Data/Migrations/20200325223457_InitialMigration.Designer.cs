4cfa9656a30f4dd350ab5cd9ad4b659d347e7729
Goran Ćudina <goran.cudina@outlook.com>
2020-03-25 23:46:32 +0100

    ok1

--- CleanArch.MVC/Data/Migrations/20200325223457_InitialMigration.Designer.cs
+++ CleanArch.MVC/Data/Migrations/20200325223457_InitialMigration.Designer.cs
@@ -4,14 +4,16 @@ using CleanArch.MVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArch.MVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200325223457_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
