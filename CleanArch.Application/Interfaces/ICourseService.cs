f7f526b3c2914a69a2fc990008acf782b8b08ffc
Goran Ćudina <goran.cudina@outlook.com>
2020-03-26 19:03:27 +0100

    Services

---
+++ CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CleanArch.Application.ViewModels;
+using System.Text;
+
+namespace CleanArch.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        public IEnumerable<CourseViewModel> GetCourses();
+    }
+}
