b7c0689e05a5d448c23cef859c92f47ccc5651e8
Goran Ćudina <goran.cudina@outlook.com>
2020-03-27 09:06:08 +0100

    final

--- CleanArch.MVC/Controllers/CourseController.cs
+++ CleanArch.MVC/Controllers/CourseController.cs
@@ -1,9 +1,11 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArch.MVC.Controllers
 {
+    [Authorize]
     public class CourseController : Controller
     {
         private ICourseService _courseService;
